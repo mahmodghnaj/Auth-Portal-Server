a4e2ea77b8ce14505b86cf72f0ee06075665a71e
Mahmod <mahmodghnaj1@gmail.com>
2023-11-01 18:35:34 +0300

    change type

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -343,9 +343,10 @@ export class AuthService {
     const maxAgeInMilliseconds = Number(ms(maxAge));
     res.cookie(cookieName, cookieValue, {
       httpOnly: true,
-      sameSite: 'lax',
+      sameSite: 'strict',
       maxAge: maxAgeInMilliseconds,
       domain: '.onrender.com',
+      path: '/',
     });
   }
 }
